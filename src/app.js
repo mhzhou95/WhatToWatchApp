88ce14cae6310664c3a8a02706d59c28c1e3e830
unknown <minghaozhou123@gmai.com>
2019-07-06 00:54:39 -0400

    loading screen

--- src/app.js
+++ src/app.js
@@ -5,5 +5,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 ReactDOM.render(<WhatToWatchApp />, document.getElementById('app'));
 
